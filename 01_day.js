da63fe4b9a9171ea4890aeaf6922ec72504b906f
Ayodhya <ayodhyarode03>
2025-11-22 19:12:10 +0530

    Number data type and Methods

--- 01_day.js
+++ 01_day.js
@@ -42,4 +42,40 @@
 // console.log("global ", a);
 
 
+// Data  Types
 
+// let a = {
+//   a  : 10
+// }
+// console.log(typeof a)
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+
+// console.log(typeof true)
+
+
+// 1] Number
+
+// console.log(Number.isNaN("hi"))    //false
+// console.log(Number.isNaN(2))       //false
+// console.log(Number.isNaN(NaN))     //true
+
+// console.log(Number.isFinite(2))
+
+// let i = "78"
+// console.log(Number.parseInt(i))  //78
+
+// let i = "Hi"
+// console.log(Number.parseInt(i))   //NaN
+
+// let i = 45.997800
+// console.log(Number.parseFloat(i))
+
+// let i = 45.997800
+// console.log(i.toFixed(2))
+// console.log(i.toFixed(3))
+// console.log(i.toFixed(4))
+
+// let i = "hi"
+// console.log(i.toString())

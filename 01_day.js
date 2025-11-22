05c1eb118b8c2309aafc63af5646460da4aaaeea
Ayodhya <ayodhyarode03>
2025-11-22 13:38:36 +0530

    js var and const

--- 01_day.js
+++ 01_day.js
@@ -10,3 +10,36 @@
 // add()
 // console.log(a)
 
+// var a = 5;
+
+// function demo() {
+//   var a = 10;
+//   console.log("in function  ", a);
+
+//   if (true) {
+//     var a = 12;
+//      console.log("if bloack", a);
+//   }
+//   console.log("if bloack", a);
+// }
+// demo();
+// console.log("global ", a);
+
+
+// const a = 5;
+
+// function demo() {
+//   const a = 10;
+//   console.log("in function  ", a);
+
+//   if (true) {
+//     const a = 12;
+//      console.log("if bloack", a);
+//   }
+//   console.log("if bloack", a);
+// }
+// demo();
+// console.log("global ", a);
+
+
+
